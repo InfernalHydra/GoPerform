0b91cd60d5f1e861b9b37415d54849e99af82867
Ryan Chhong <ryanchhong@gmail.com>
2019-03-12 19:37:16 -0500

    half baked backend

---
+++ schema/User.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var userSchema = {
+    name: String,
+    password: String,
+    email: String,
+    phoneNumber: String,
+    socialHandle: String,
+};
+
+var User = mongoose.model("User", userSchema);
+
+module.exports = User;
